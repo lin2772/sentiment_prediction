47e10bf47e18efe2f859c63d9c161c14d4dfee27
lin2772 <46533075+lin2772@users.noreply.github.com>
2022-04-01 22:49:08 -0400

    Create prediction_via_model_url.py

---
+++ prediction_via_model_url.py
@@ -0,0 +1,16 @@
+import os
+import requests
+import numpy as np
+import pandas as pd
+
+def create_tf_serving_json(data):
+  return {'inputs': {name: data[name].tolist() for name in data.keys()} if isinstance(data, dict) else data.tolist()}
+
+def score_model(dataset):
+  url = 'https://adb-4167085237468301.1.azuredatabricks.net/model/sentiment-best-testing-accuracy/1/invocations'
+  headers = {'Authorization': f'Bearer {os.environ.get("DATABRICKS_TOKEN")}'}
+  data_json = dataset.to_dict(orient='split') if isinstance(dataset, pd.DataFrame) else create_tf_serving_json(dataset)
+  response = requests.request(method='POST', headers=headers, url=url, json=data_json)
+  if response.status_code != 200:
+    raise Exception(f'Request failed with status {response.status_code}, {response.text}')
+  return response.json()
