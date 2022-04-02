f8f9fc3dbf3f0c8186cb02f2debe5069ce4fabe8
EC2 Default User <ec2-user@ip-172-31-92-111.ec2.internal>
2022-04-02 18:00:16 +0000

    model locally working

--- predict_panda.py
+++ predict_panda.py
@@ -1,11 +1,14 @@
 import mlflow
+import json
 import pandas as pd
+from pandas.io.json import json_normalize
+
 
 # Load model as a PyFuncModel.
 loaded_model = mlflow.pyfunc.load_model('model')
 
-# Predict on a Pandas DataFrame.
+data = [[".@user holds the fate of the republican pay in his tiny, tiny, tiny, little hands.   #gop"], ["@user +zig zag way is show 2b proud of top cast+great backstage team lovely script awesome music #encore #cornish #powerful #funny  "], ["latepost\u00f0\u009f\u0091\u00bb\u00f0\u009f\u0094\u00ab #like #like4like #likeforlike  #goodnight #night  #face #happy    #latepost  #sendiri "], ["ocean commotion!   #vbs2016 @user "], ["you look sexy af and fine @user \u00f0\u009f\u0098\u008d\u00f0\u009f\u0098\u008d\u00f0\u009f\u0098\u008d\u00e2\u009d\u00a4\u00ef\u00b8\u008f\u00f0\u009f\u0098\u0089\u00f0\u009f\u0098\u008b "]]
 
+dp= pd.DataFrame(data, columns = ["tweet"])
 
-data = pd.read_json('model/input_example.json')
-print(loaded_model.predict(pd.DataFrame(data)))
+print(loaded_model.predict(dp))
