6e5774f6c6d0287af922a37a8528ca7e5e7c057f
Weilie <lin.2772@osu.edu>
2022-04-03 02:16:27 +0000

    modify fastapi

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 import uvicorn
+import pandas as pd
+import mlflow
+import json
+from pandas.io.json import json_normalize
+
+loaded_model = mlflow.pyfunc.load_model('../model')
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "This is IDS721 22Spring Group Project. Weilie Lin, Bohan Li, Yubo Ding"}
-    
+
+@app.get("/columns/{col}/data/{dat}")
+async def callModel(col: str, dat: str):
+    dp= pd.DataFrame(eval(dat), columns = [col])
+    return(loaded_model.predict(dp))
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0') 
