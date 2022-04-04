3299ebbddd6b2d34f0174d828c1f610aba5316c6
Weilie <lin.2772@osu.edu>
2022-04-04 17:34:20 +0000

    add form

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,20 @@
-FROM python:3.8.12
+FROM public.ecr.aws/lambda/python:3.8
 
 RUN mkdir -p /app
+
+
+
+# Copy source code to working directory
+COPY . fastapi/main.py /app/fastapi/
+COPY . model /app/model/
+COPY . requirements.txt /app/
 
 # Working Directory
 WORKDIR /app
 
-# Copy source code to working directory
-COPY . main.py /app/
-COPY . requirements.txt /app/
-
 # Install packages from requirements.txt
-RUN pip install --upgrade pip &&\
-    pip install --trusted-host pypi.python.org -r /app/requirements.txt
+RUN pip install -r requirements.txt
     
-
+EXPOSE 8080
 ENTRYPOINT ["python"]
-CMD ["/app/fastapi/main.py"]
+CMD ["fastapi/main.py"]
--- fastapi/main.py
+++ fastapi/main.py
@@ -1,22 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 import uvicorn
 import pandas as pd
 import mlflow
 import json
 from pandas.io.json import json_normalize
+from starlette.templating import Jinja2Templates
+from starlette.requests import Request
+from pathlib import Path
 
-loaded_model = mlflow.pyfunc.load_model('../model')
+BASE_DIR = Path(__file__).resolve().parent
 
-app = FastAPI()
+templates = Jinja2Templates(directory=str(Path(BASE_DIR, 'templates')))
+loaded_model = mlflow.pyfunc.load_model('model')
+app = FastAPI(template_folder='template')
 
 @app.get("/")
-async def root():
-    return {"message": "This is IDS721 22Spring Group Project. Weilie Lin, Bohan Li, Yubo Ding"}
+async def root(request: Request):
+    return templates.TemplateResponse('post.html', { 'request': request})
+    # return {"message": "This is IDS721 22Spring Group Project. Weilie Lin, Bohan Li, Yubo Ding"}
 
 @app.get("/columns/{col}/data/{dat}")
 async def callModel(col: str, dat: str):
+    #col = tweet
+    #data = ['you suck']
+    
     dp= pd.DataFrame(eval(dat), columns = [col])
     return {"results": str(loaded_model.predict(dp))}
+
+@app.post("/input/")
+async def inputForm(request: Request, username: str = Form(...), password: str = Form(...)):
+    dp= pd.DataFrame(eval(password), columns = [username])
+    return {"results": str(loaded_model.predict(dp))}
+    # return templates.TemplateResponse('post.html', {
+    # 'request': request, user})
     
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0') 
